63a6b9bddbfa216ac1763c55cf46fec79f4db4f2
delvinj <delvinj@gmail.com>
2013-12-28 12:36:30 -0600

    Support IFrames

--- jquery-event-spy.js
+++ jquery-event-spy.js
@@ -1,37 +1,58 @@
-// :noTabs=false:mode=javascript:tabSize=2:indentSize=2:folding=indent:
+// :noTabs=true:mode=javascript:tabSize=2:indentSize=2:folding=indent:
 
 /**
  * Captures jQuery events in the console. Invoke once to start, again to stop.
  */
 (function() {
 	var triggerId = "~jQuery.event.trigger";
+	var counterId = triggerId + ".count";
 	
-	if (window[triggerId] !== undefined) {
-		/* uninstall */
-		jQuery.event.trigger = window[triggerId];
+	function loggingTrigger() {
+		var originalTrigger = this[triggerId];jq
+		var count = this[counterId];
+		if (count === undefined) {
+			count = 0;
+		}
+		/* Go fishing for some common event types/info. */
+		var type, arr;
+		if (arguments[0] instanceof this.jQuery.Event) {
+			type = arguments[0].type || "jQuery.Event";
+			arr = [].slice.call(arguments, 1);
+		} else if (arguments[0] && arguments[0].constructor === String) {
+			type = arguments[0];
+			arr = [].slice.call(arguments, 1);
+		} else {
+			type = "Unknown";
+			arr = [].slice.call(arguments);
+		}
+		(window.console && console.debug && console.debug(count, type, arr));
+		this[counterId] = ++count;
 		
-		window[triggerId] = undefined;
-	} else {
-		/* install */
-		var originalTrigger = jQuery.event.trigger;
-		var count = 0;
-		
-		window[triggerId] = originalTrigger;
-		
-		jQuery.event.trigger = function() {
-			++count;
-			if (arguments[0] instanceof jQuery.Event) {
-				var type = arguments[0].type || "jQuery.Event";
-				var a = [].slice.call(arguments, 1);
-			} else if (arguments[0] && arguments[0].constructor === String) {
-				var type = arguments[0];
-				var a = [].slice.call(arguments, 1);
-			} else {
-				var type = "Unknown";
-				var a = [].slice.call(arguments);
-			}
-			(window.console && console.info && console.info(count, type, a));
-			return originalTrigger.apply(arguments.callee.caller, arguments);
-		};
+		return originalTrigger.apply(arguments.callee.caller, arguments);
 	}
+	
+	function installOrRemove(scope) {
+		try {
+			if (triggerId in scope) {
+				scope.jQuery.event.trigger = scope[triggerId];
+				delete scope[triggerId];
+				delete scope[counterId];
+			} else {
+				var original = scope.jQuery.event.trigger;
+				scope[triggerId] = original;
+				scope.jQuery.event.trigger = loggingTrigger.bind(scope);
+			}
+		} catch (x) {
+			(window.console && console.error && console.error(x));
+		}
+	}
+	
+	if (window.frames.length >= 1) {
+		for (var j=0; j < window.frames.length; ++j) {
+			installOrRemove(window.frames[j]);
+		}
+	} else {
+		installOrRemove(window);
+	}
+	
 })();
